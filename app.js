7ba4410dad8a287135e127ff3e74d4a139893f98
Bitrey <info@bitrey.it>
2021-02-23 11:04:20 +0100

    more emojis

--- app.js
+++ app.js
@@ -58,14 +58,68 @@ client.on(`message`, message => {
         const catt1 = message.guild.emojis.cache
             .find(emoji => emoji.name === "catt1")
             .toString();
+        const catt7 = message.guild.emojis.cache
+            .find(emoji => emoji.name === "catt1")
+            .toString();
+        const alepanco = message.guild.emojis.cache
+            .find(emoji => emoji.name === "alepanco")
+            .toString();
+        const ff1 = message.guild.emojis.cache
+            .find(emoji => emoji.name === "alewide3")
+            .toString();
+        const ff2 = message.guild.emojis.cache
+            .find(emoji => emoji.name === "alewide2")
+            .toString();
+        const ff3 = message.guild.emojis.cache
+            .find(emoji => emoji.name === "alewide1")
+            .toString();
+        const jj = message.guild.emojis.cache
+            .find(emoji => emoji.name === "jjgaming2")
+            .toString();
+        const catty = message.guild.emojis.cache
+            .find(emoji => emoji.name === "catty1")
+            .toString();
+        const furry = message.guild.emojis.cache
+            .find(emoji => emoji.name === "mrfurry1")
+            .toString();
+        const rxsty = message.guild.emojis.cache
+            .find(emoji => emoji.name === "rxstyemote2")
+            .toString();
+        const ivan = message.guild.emojis.cache
+            .find(emoji => emoji.name === "ivanetor2")
+            .toString();
 
         // Fa schifo
         if (msg == "muztika") message.channel.send("fa schifo", { tts: true });
         else if (msg == "sevy") message.channel.send("gaming");
+        else if (msg == "seba") message.channel.send("cassa");
         else if (msg == "reb" || msg == "rebz")
             message.channel.send(occhioreb || "non trovo l'emoji :(");
         else if (msg == "bitrey")
             message.channel.send(catt1 || "non trovo l'emoji :(");
+        else if (msg == "alepanco")
+            message.channel.send(alepanco || "non trovo l'emoji :(");
+        else if (msg == "fiocchetto" || msg == "focchietto")
+            message.channel.send(catt7 || "non trovo l'emoji :(");
+        else if (msg == "ff")
+            message.channel.send(
+                ff1 ? `${ff1}${ff2}${ff3}` : "non trovo l'emoji :("
+            );
+        else if (msg == "jj" || msg == "jojax" || msg == "jojaxgaming")
+            message.channel.send(jj || "non trovo l'emoji :(");
+        else if (msg == "catty")
+            message.channel.send(catty || "non trovo l'emoji :(");
+        else if (msg == "furry" || msg == "mrfurry" || msg == "carrisi")
+            message.channel.send(furry || "non trovo l'emoji :(");
+        else if (msg == "andre" || msg == "rxsty")
+            message.channel.send(rxsty || "non trovo l'emoji :(");
+        else if (
+            msg == "ivan" ||
+            msg == "iv4n" ||
+            msg == "ivanetor" ||
+            msg == "iv4netor"
+        )
+            message.channel.send(ivan || "non trovo l'emoji :(");
     }
 });
 
